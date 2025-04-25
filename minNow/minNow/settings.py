40d3c0a63f0b648f1f3886bab09429803962a7be
aysmith17 <aysmith17@gmail.com>
2025-04-25 00:25:28 -0600

    update settings to integrate WhiteNoise for static file handling in production. Ran collecstatic

--- minNow/minNow/settings.py
+++ minNow/minNow/settings.py
@@ -136,9 +136,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
-
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
@@ -150,3 +151,6 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 CORS_ALLOW_CREDENTIALS = True
+
+# Add this line to ensure WhiteNoise works in production
+MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")

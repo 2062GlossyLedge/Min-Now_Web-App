83240d87d6c41d7f57fb44f1aad90b889ba1b622
aysmith17 <aysmith17@gmail.com>
2025-05-21 00:34:24 -0600

    Revert "moved auth since import statement for auth wasn't working in both prod and local, but only local"
    This reverts commit 239b0d50c503bf8c5523f35bbea7662f37dff918.

--- minNow/minNow/settings.py
+++ minNow/minNow/settings.py
@@ -22,7 +22,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Add the project root directory to Python path
-sys.path.insert(0, str(BASE_DIR.parent))  # Add the parent directory of minNow
+sys.path.append(str(BASE_DIR))
 
 
 # Quick-start development settings - unsuitable for production
@@ -104,7 +104,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "corsheaders",
-    "core",
     "items",
     "users",
 ]

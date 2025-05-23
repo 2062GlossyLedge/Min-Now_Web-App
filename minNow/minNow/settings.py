4b520fa62f21b17ab12bd6aaa31a4d3b29319ae9
aysmith17 <aysmith17@gmail.com>
2025-05-22 20:00:53 -0600

    reapplied env names

--- minNow/minNow/settings.py
+++ minNow/minNow/settings.py
@@ -87,11 +87,11 @@ if prod:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.getenv("PPGDATABASE"),
-            "USER": os.getenv("PPGUSER"),
-            "PASSWORD": os.getenv("PPGPASSWORD"),
-            "HOST": os.getenv("PPGHOST", "localhost"),
-            "PORT": os.getenv("PPGPORT", "5432"),
+            "NAME": os.getenv("PGDATABASE"),
+            "USER": os.getenv("PGUSER"),
+            "PASSWORD": os.getenv("PGPASSWORD"),
+            "HOST": os.getenv("PGHOST", "localhost"),
+            "PORT": os.getenv("PGPORT", "5432"),
             "OPTIONS": {
                 "sslmode": "require",
             },

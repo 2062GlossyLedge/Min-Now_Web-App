619e446d7e298e0d1697bb9dabf05102f5474451
aysmith17 <aysmith17@gmail.com>
2025-08-17 19:44:00 -0600

    csp implemented.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
     children: React.ReactNode
 }) {
     return (
-        <ClerkProvider>
+        <ClerkProvider dynamic>
             <html lang="en" suppressHydrationWarning>
                 <body className={inter.className}>
                     {/* HydrationWrapper mounts first, blocking all children until mounted */}

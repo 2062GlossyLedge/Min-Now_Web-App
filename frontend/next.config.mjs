a8026df40488688c9d7ec5e15ae8ebbe4a660ca9
aysmith17 <aysmith17@gmail.com>
2025-12-07 11:20:47 -0700

    updated middleware file to new proxy name, updated node js deps. removed depcraed es lint config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -25,9 +25,6 @@ const config = {
     typescript: {
         ignoreBuildErrors: true,
     },
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
     async rewrites() {
         return [
             {
